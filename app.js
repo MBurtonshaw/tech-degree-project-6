a2f0bed3092843ca363e31ba5fe796d987c9d54f
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2021-02-01 12:23:12 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
 const express = require("express");
 const app = express();
-const infoFile = require("./data.json");
-const { data } = infoFile;
+const { data } = require("./data.json");
+const { projects } = { data };
 const bodyParser = require("body-parser");
+
+
+
 app.use(bodyParser.urlencoded({extended: false}))
 app.use("/static", express.static("public"));
 
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
-    res.render("index", {
-        data
-        });
+    res.render("index", { projects });
 });
 
 app.get("/about", (req, res) => {
-    res.render("about");
+    res.render("about", { projects });
 });
 
 app.get('/projects/:id', (req, res, next) => {
   // Log out home route handler indication
-    res.render("project", data.projects[req.params.id]);
+    const { id } = req.params;
+    const project = projects[id];
+    res.render("project", { project });
 });
 
 app.listen("3000", () => {
     console.log("running");
 });
 
-module.exports = app;
